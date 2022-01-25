fe3ae8a38f7105d54ad6bf1469b2a23d9c32c8cf
Josh Wu <joshwu@google.com>
2022-01-24 22:56:14 -0800

    sdm660-common: Switch BtAudio to AIDL
    Test: m
    Bug: 203490261
    Change-Id: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7
    Merged-In: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7

--- sdm660.mk
+++ sdm660.mk
@@ -75,7 +75,8 @@ PRODUCT_PACKAGES += \
     libvolumelistener
 
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.audio@2.0-impl:32 \
+    android.hardware.bluetooth.audio-impl:32 \
+    android.hardware.bluetooth@1.0.vendor \
     audio.bluetooth.default
 
 PRODUCT_PACKAGES += \
